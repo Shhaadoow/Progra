39c83c895c5f9edd470e3ef6a790651603d82530
Pablo Ordóñez <pablosebas61@gmail.com>
2025-11-14 07:50:09 -0500

    Cambios ejercicio 1

---
+++ Soluciones/src/main.scala
@@ -0,0 +1,5 @@
+def prom(datos: List[Double]): Double = {
+  val promedio = datos.sum / datos.size
+  val sumaCuadrados = datos.map(x => math.pow(x - promedio, 2)).sum
+  math.sqrt(sumaCuadrados / datos.size)
+}
